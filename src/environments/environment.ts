4e24f36dba7851cc8d2318b9f46c7479942ff002
dia eddine <diaeddinee@outlook.fr>
2024-12-10 19:18:15 +0100

    Deleted some ununsed images

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    YOUR_SERVICE_ID:'service_tdsgrmf',
-    YOUR_TEMPLATE_ID:'template_4n3v78r',
+    YOUR_SERVICE_ID:'service_uvqx1vk',
+    YOUR_TEMPLATE_ID:'template_hqe90bd',
     YOUR_PUBLIC_KEY:'Ar38p1uX-gu3jqoW0',
 };
